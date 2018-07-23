c33b0bd449f876f5be3afb113c030f63e0673c5d
Jan Mewes <jan.mewes@ks-plus.org>
2018-07-23 19:46:06 +0200

    WIP

--- user-interface/src/main/java/ksch/registration/RegisterPatientActivity.java
+++ user-interface/src/main/java/ksch/registration/RegisterPatientActivity.java
@@ -2,6 +2,7 @@ package ksch.registration;
 
 import ksch.ApplicationFrame;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.wicketstuff.annotation.mount.MountPath;
 
@@ -11,5 +12,13 @@ public class RegisterPatientActivity extends ApplicationFrame {
 
     public RegisterPatientActivity(PageParameters pageParameters) {
         super(pageParameters);
+
+        WebMarkupContainer patientListContainer = new WebMarkupContainer("patientList");
+
+
+        add(patientListContainer);
+
+        patientListContainer.setVisible(true);
+
     }
 }
