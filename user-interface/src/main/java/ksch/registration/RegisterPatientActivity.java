7b11832063731150e8bfb3636ad6e52440629932
Jan Mewes <jan.mewes@ks-plus.org>
2018-07-24 15:38:47 +0200

    WIP

--- user-interface/src/main/java/ksch/registration/RegisterPatientActivity.java
+++ user-interface/src/main/java/ksch/registration/RegisterPatientActivity.java
@@ -1,24 +1,47 @@
 package ksch.registration;
 
 import ksch.ApplicationFrame;
+import lombok.extern.java.Log;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.wicketstuff.annotation.mount.MountPath;
 
 @MountPath("/registration/register-patient")
 @AuthorizeInstantiation({"NURSE", "CLERK"})
+@Log
 public class RegisterPatientActivity extends ApplicationFrame {
+
+    private String patientSearchTerm = null;
 
     public RegisterPatientActivity(PageParameters pageParameters) {
         super(pageParameters);
 
         WebMarkupContainer patientListContainer = new WebMarkupContainer("patientList");
 
-
         add(patientListContainer);
+        patientListContainer.setVisible(false);
 
-        patientListContainer.setVisible(true);
+        PropertyModel<String> patientSearchTermModel = new PropertyModel<>(this, "patientSearchTerm");
 
+        Form<?> patientSearchForm = new Form<Void>("patientSearchForm") {
+
+            @Override
+            protected void onSubmit() {
+                String patientSearchTerm = patientSearchTermModel.getObject();
+
+                patientListContainer.setVisible(true);
+                log.info(patientSearchTerm);
+
+                patientSearchTermModel.setObject(null);
+            }
+        };
+
+        patientSearchForm.add(new TextField("patientSearchTerm", patientSearchTermModel));
+
+        add(patientSearchForm);
     }
 }
